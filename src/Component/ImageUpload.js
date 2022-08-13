75114bdda397f08a11b3a60b52832cd9cf7dd194
Pro-Developer01 <yadavshashank70@gmail.com>
2022-08-14 03:20:29 +0530

    Api, fetch, error response added

--- src/Component/ImageUpload.js
+++ src/Component/ImageUpload.js
@@ -1,25 +1,42 @@
 import React, { useState } from 'react'
+import "./Style.css";
 
 export default function ImageUpload() {
     const [Image,setImage]=useState([]);
     const fileHandler=(e)=>{
-        e.preventDefault();
         console.log(e.target.files);
-        setImage(e.target.files);
+        setImage(e.target.files[0]);
         console.log("image",Image);
     }
 
-    const UploadHandler=()=>{
+    const UploadHandler=(e)=>{
+        e.preventDefault();
         console.log("clicked");
+        const url="https://api.imgur.com/3/image/";
+        const formdata=new FormData();
+        formdata.append('image',Image);
+        fetch(url,{
+            method:'POST',
+            headers:{
+                Authorization: "Client-ID b8a32186230e488"
+            },
+            body: formdata
+        }).then(data=>data.json()).then(data=>console.log(data)).catch(e=>console.log(e))
     }
   return (
-    <div>
+    <div className='container'>
         <form>
+            <div className="input">
+
             <label htmlFor="Image">
                 Upload Image
             </label>
             <input type="file" id="Image"onChange={fileHandler} />
+            </div>
+            <div className="button">
+
             <button onClick={UploadHandler}>Upload</button>
+            </div>
         </form>
     </div>
   )
