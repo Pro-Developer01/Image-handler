863e4b830eea1c5a9308c791897b32d3d29e1ff9
Pro-Developer01 <yadavshashank70@gmail.com>
2022-08-14 18:46:49 +0530

    Bootstrap Integration

--- src/Component/ImageUpload.js
+++ src/Component/ImageUpload.js
@@ -2,72 +2,94 @@ import React, { useState } from 'react'
 import "./Style.css";
 
 export default function ImageUpload() {
-    const [Image,setImage]=useState([]);
-    const [dataget,setdataget]=useState([]);
-    const [imglink,setimglink]=useState("");
-    const fileHandler=(e)=>{
+    const [Image, setImage] = useState([]);
+    const [dataget, setdataget] = useState([]);
+    const [imglink, setimglink] = useState("");
+    const fileHandler = (e) => {
         console.log(e.target.files);
         setImage(e.target.files[0]);
-        console.log("image",Image);
+        console.log("image", Image);
     }
 
-    const UploadHandler=(e)=>{
+    const UploadHandler = (e) => {
         e.preventDefault();
         console.log("clicked");
-        const url="https://api.imgur.com/3/image/";
-        const formdata=new FormData();
-        formdata.append('image',Image);
-        fetch(url,{
-            method:'POST',
-            headers:{
+        const url = "https://api.imgur.com/3/image/";
+        const formdata = new FormData();
+        formdata.append('image', Image);
+        fetch(url, {
+            method: 'POST',
+            headers: {
                 Authorization: "Client-ID b8a32186230e488"
             },
             body: formdata
-        }).then(data=>data.json()).then((z)=>{
+        }).then(data => data.json()).then((z) => {
             console.log(z)
             setdataget(z);
-        }).catch(e=>console.log(e))
+        }).catch(e => console.log(e))
     }
-    const DownloadHandler=(e)=>{
+    const DownloadHandler = (e) => {
         e.preventDefault();
-        const url="https://api.imgur.com/3/account/me/images";
-        fetch(url,{
-            method:"GET",
-            headers:{
-                Authorization:"Client-ID b8a32186230e488"
+        const url = "https://api.imgur.com/3/account/me/images";
+        fetch(url, {
+            method: "GET",
+            headers: {
+                Authorization: "Client-ID b8a32186230e488"
             },
         })
-        .then(data=>data.json()).then(data=>console.log(data)).catch(e=>console.log(e));
+            .then(data => data.json()).then(data => console.log(data)).catch(e => console.log(e));
     }
-   
-    const ShowImages=(e)=>{
+
+    const ShowImages = (e) => {
         e.preventDefault();
         console.log("clicked");
-       setimglink( dataget.data.link);
+        console.log(dataget.length);
+        setimglink(dataget.data.link);
     }
-  return (
-    <div className='container'>
-        <form>
-            <div className="input">
+    return (
+        <div className='container'>
+            <center><h1>Upload Image</h1></center>
+            <br />
+            <form>
+                <div className="input">
+                    <div className="input-group flex-nowrap">
+                        <span className="input-group-text" id="addon-wrapping" style={{
+                            fontWeight: "600",
+                        }}> <>Upload Image </></span>
+                        <input onChange={fileHandler} type="file" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping" />
+                    </div>
+                </div>
+                <br />
+                <div className="button">
+                    <button type="button" className="btn btn-primary" onClick={UploadHandler}>Upload</button>
+                </div>
 
-            <label htmlFor="Image">
-                Upload Image
-            </label>
-            <input type="file" id="Image"onChange={fileHandler} />
-            </div>
-            <div className="button">
 
-            <button onClick={UploadHandler}>Upload</button>
-            </div>
-            <button onClick={DownloadHandler}>Get Images</button>
-            <button onClick={ShowImages}>Show Images</button>
-        </form>
-        {dataget &&(
-            <>
-        <hr></hr>
-        <img src={imglink} alt="notfound" style={{height: "400px", width:"200px"}} />
-            </>
-        )}
-    </div>
-  )
+            </form>
+            <hr></hr>
+            {/* <button type="button" onClick={DownloadHandler} className="btn btn-primary" onClick={UploadHandler}>Get Images</button> */}
+
+            <center>
+                <button type="button" onClick={ShowImages} className="btn btn-primary">Show Images</button>
+            </center>
+            {dataget.length > 0 && (
+                <>
+                    <center>
+                        <div className="card" style="width: 18rem;">
+                            <img src={imglink} className="card-img-top" alt="notfound" />
+                            <div className="card-body">
+                                <h6 className="card-title"><h5>Type: </h5> {dataget.data.type}</h6>
+                                <h6 className="card-title"><h5>Dimension: </h5> {dataget.data.width} X {dataget.data.height}</h6>
+                                <h6 className="card-title"><h5>View: </h5> {dataget.data.views}</h6>
+
+                                {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
+                            </div>
+                        </div>
+
+                        {/* <img src=  style={{ height: "400px", width: "200px" }} /> */}
+                    </center>
+                </>
+            )}
+        </div>
+    )
 }
