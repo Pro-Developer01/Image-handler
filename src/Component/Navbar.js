b82a6b42a51a0424f4aad0b0e38fce54f4ed5829
Pro-Developer01 <yadavshashank70@gmail.com>
2022-08-14 18:58:12 +0530

    warning Fixes

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
     
       <nav className="navbar navbar-expand-lg bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="/">Navbar</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -13,7 +13,7 @@ export default function Navbar() {
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         
         <li className="nav-item dropdown">
-          <a className="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" >
+          <a className="nav-link active dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false" >
             Other Projects
           </a>
           <ul className="dropdown-menu">
